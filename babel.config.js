0b236ea15879a52d0ac9e8cd4cab7633e2ff3930
詹志强 <zqzhan@kaikeba.com>
2021-02-09 15:59:04 +0800

    feat: 添加babel兼容处理

--- babel.config.js
+++ babel.config.js
@@ -13,8 +13,21 @@ module.exports = {
     '@babel/preset-typescript',
   ],
   plugins: [
-    '@babel/proposal-class-properties',
+    '@babel/plugin-transform-react-jsx',
+    '@babel/plugin-transform-runtime',
     '@babel/proposal-object-rest-spread',
-    ['@babel/plugin-transform-runtime', { helpers: false }],
+    '@babel/plugin-syntax-dynamic-import', // 动态导入
+    '@babel/plugin-syntax-import-meta',
+    '@babel/plugin-proposal-function-sent', // 转换成es5
+    ['@babel/plugin-proposal-class-properties', { loose: true }],
+    '@babel/plugin-proposal-export-namespace-from',
+    // 转换语法处理
+    '@babel/plugin-transform-destructuring', // 结构赋值
+    '@babel/plugin-transform-arrow-functions', // 箭头函数
+    '@babel/plugin-transform-async-to-generator', // 二个插件解决async语法问题
+    '@babel/plugin-transform-regenerator',
+    '@babel/plugin-proposal-numeric-separator',
+    '@babel/plugin-proposal-throw-expressions',
+    '@babel/plugin-transform-template-literals', // 字符串模板
   ],
 };
